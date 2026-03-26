1621c8f7e9cb64d8c3fce968365498bf59917410
Areeb <49764279+AreebEhsan@users.noreply.github.com>
2026-03-25 21:21:17 -0400

    Go syntax fundamentals practice through algorithm implementation

---
+++ algorithms/arrays/two_sum.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func twoSum(arr []int, target int) [] int {
+
+	hash := make(map[int]int)
+
+	for i, v := range arr{
+		diff  := target- v
+
+		if idx, found := hash[diff]; found{
+			return [] int{idx, i}
+		}
+		hash[v] = i
+	}
+	return nil
+}
+
+func main(){
+
+	arr:= []int{12,653,1245,7543,1223,-1234,54}
+	target := -11
+
+	result := twoSum(arr, target)
+	fmt.Println("Indices", result)
+}
